67093f17e2929a3be191b35c901652bf9663b093
mathekalawrence <mathekalawrence96@gmail.com>
2025-09-08 22:53:52 +0300

    My Second Commit

---
+++ TheStarter/app/(tabs)/navigation.ts
@@ -0,0 +1,14 @@
+
+export type RootStackParamList = {
+    Welcome: undefined;
+    SignUp: undefined;
+    SignIn: undefined;
+    Home: undefined;
+};
+
+export type User = {
+    username: string;
+    email: string;
+    identityNumber: string;
+    password: string;
+};
